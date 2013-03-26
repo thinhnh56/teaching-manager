8fd93d022b5c1c588d23979c8633872e5da6df6b
LUIS NGO <baoloc1993@gmail.com>
2013-03-26 23:24:23 +0700

    update setting_online

--- teaching_management/setting_online.py
+++ teaching_management/setting_online.py
@@ -134,6 +134,7 @@ LOGGING = {
     'handlers': {
         'mail_admins': {
             'level': 'ERROR',
+			'filters': '',
             'class': 'django.utils.log.AdminEmailHandler'
         }
     },
