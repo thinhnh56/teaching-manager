2bd98287feca99849870613b386b8b6b3882c96e
anhnh55 <nha.2731991@gmail.com>
2013-03-26 19:12:56 +0700

    test login site
    to run test: "python manage.py test test_login_site"

---
+++ teaching_management/test_login_site/tests.py
@@ -0,0 +1,34 @@
+from django.test import LiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+class viewsTest(LiveServerTestCase):
+    fixtures = ['user.json']
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+    
+    def test_can_open_login_site(self):
+        # An user open web browser and can open the login site's url
+        self.browser.get(self.live_server_url + '/login/')
+        
+        # He sees 'login site for non-staff user'
+        body = self.browser.find_element_by_tag_name('body')
+        self.assertIn('Enter your username and password to login', body.text)
+        
+        # He looks for username field
+        username_field = self.browser.find_element_by_name('username')
+        # He looks for password field
+        password_field = self.browser.find_element_by_name('password')
+        
+        # He types in his username and password then hit Enter
+        username_field.send_keys('abcd')
+        password_field.send_keys('1234')
+        password_field.send_keys(Keys.RETURN)
+        
+        # He logged in successfully
+        body = self.browser.find_element_by_tag_name('body')
+        self.assertIn('You have logged in successfully', body.text)
