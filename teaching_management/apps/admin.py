4e4f377d87e2a1486f1fd0ea45cc6384ef6811ba
Nguyễn Hưng Thịnh <thinhnh56@gmail.com>
2013-04-06 13:55:03 +0700

    register model to admin site

--- teaching_management/apps/admin.py
+++ teaching_management/apps/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from apps.models import Program, Subject, Lecturer
+from apps.models import Program, Subject, Lecturer, Scheduler, Scheduler_link
 
 admin.site.register(Program)
 admin.site.register(Subject)
 admin.site.register(Lecturer)
+admin.site.register(Scheduler)
+admin.site.register(Scheduler_link)
