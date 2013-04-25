091eab1e42778e8d3f47c9e9b21b4792af6609c8
Nguyễn Hưng Thịnh <thinhnh56@gmail.com>
2013-04-25 07:35:45 +0700

    add statistic view and refactor

--- teaching_management/apps/models.py
+++ teaching_management/apps/models.py
@@ -1,60 +1,93 @@
+"""
+    this file define models for database
+"""
 from django.db import models
 
-# Create your models here.
 
-		
 class Program(models.Model):
-	name = models.CharField(max_length = 30)
-	credits = models.IntegerField()
-	
-	def __unicode__(self):
-                return self.name
+    """
+       program model contain imfomation about a program:
+       name : name of a program
+       credit: amount of credit that assigned to a program   
+    """
+    name = models.CharField(max_length = 30)
+    credits = models.IntegerField()
+     
+    def __unicode__(self):
+        return self.name
 
-	
-#Subject field includes :
-##Name of subject
-##ID of subject
-##Program which subject belong to
-##Number of credits
- 
+    
+
 class Subject(models.Model):
-	name = models.CharField(max_length = 30)
-	ID = models.CharField(max_length = 10, primary_key = True)
-	program = models.ForeignKey(Program)
-	credits = models.IntegerField()
-	
-	def __unicode__(self):
-		return self.name
+    """Subject field includes :
+    Name of subject
+    ID of subject
+    Program which subject belong to
+    Number of credits
+    """
+    name = models.CharField(max_length = 30)
+    ID = models.CharField(max_length = 10, primary_key = True)
+    program = models.ForeignKey(Program)
+    credits = models.IntegerField()
+    
+    def __unicode__(self):
+        return self.name
 
-#Lecturer field includes :
-##Name of lecturer
-##Faculty where Lecturers are working
-##Subjects which a lecturer	are teaching
-##Another Subjects which a lecturer can teach
-##Number of credits that lecturer are teaching
+
 class Lecturer(models.Model):
-	name = models.CharField(max_length = 30)
-	falcuty = models.CharField(max_length = 50)
-	subjects_in_charge = models.ManyToManyField(Subject, related_name = 'c+')
-	subjects_can_teach = models.ManyToManyField(Subject, related_name = 't+')
-	credits = models.IntegerField()
-	def __unicode__(self):
-		return self.name
+    """
+    Lecturer field includes :
+    Name of lecturer
+    Faculty where Lecturers are working
+    Subjects which a lecturer    are teaching
+    Another Subjects which a lecturer can teach
+    Number of credits that lecturer are teaching
+    """
+    name = models.CharField(max_length = 30)
+    faculty = models.CharField(max_length = 50)
+    subjects_in_charge = models.ManyToManyField(Subject, related_name = 'c+')
+    subjects_can_teach = models.ManyToManyField(Subject, related_name = 't+')
+    credits = models.IntegerField()
+    def __unicode__(self):
+        return self.name
+    
 
-#Scheduler field includes:
-##Name of Scheduler
 class Scheduler(models.Model):
-	name = models.CharField(max_length = 30)
+    """
+    Scheduler field includes:
+    Name of Scheduler
+    """
+    name = models.CharField(max_length = 30)
+    
+    def __unicode__(self):
+        return self.name
 
-	def __unicode__(self):
-		return self.name
-
-#Scheduler_link field includes:
-##scheduler id
-##subject
-##lecturer
-##model to connet
 class Scheduler_link(models.Model):
-	scheduler = models.ForeignKey(Scheduler)
-	subject = models.ForeignKey(Subject)
-	lecturer = models.ManyToManyField(Lecturer)
+    """
+    Scheduler_link field includes:
+    scheduler id
+    subject
+    lecturer
+    model to connet
+    """
+    scheduler = models.ForeignKey(Scheduler)
+    subject = models.ForeignKey(Subject)
+    lecturer = models.ManyToManyField(Lecturer)
+
+
+class Lecturer_teaching_info(models.Model):
+    """
+    this model store infomation about
+        subjects that Lecturer has taught
+    """
+    lecturer = models.ForeignKey(Lecturer)
+    subject = models.ManyToManyField(Subject)
+    credit = models.IntegerField()
+    
+    def update(self, subject):
+        self.subject.add(subject)
+        self.credit = self.credit + subject.credits
+        self.save()
+        
+    def __unicode__(self):
+        return self.lecturer.name
