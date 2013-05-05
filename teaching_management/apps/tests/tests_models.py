750cc7363e8395d87cf904767f6c9341327bdd10
4ever1lov3 <tuananhhoangcong@gmail.com>
2013-05-05 21:09:11 +0700

    refactoring

--- teaching_management/apps/tests/tests_models.py
+++ teaching_management/apps/tests/tests_models.py
@@ -1,22 +1,9 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
 from apps.models import *
-
-# class SimpleTest(TestCase):
-# 	def test_basic_addition(self):
-# 		"""
-# 		Tests that 1 + 1 always equals 2.
-# 		"""
-# 		self.assertEqual(1 + 1, 2)
 		
 class TestTheModels(TestCase):
 	def test_adding_program(self):
+		"""test adding a program"""
 		def setup(self):
 			self.program = Program(name='ISP', credits=152)
 		def test(self):
@@ -26,9 +13,11 @@ class TestTheModels(TestCase):
 		
 	def test_adding_Subject(self):
 		def setup(self):
+			"""adding a subject"""
 			self.subject = Subject(name='SE', ID='INT1111', credits=3)
 		
 		def test(self):
+			"""test asserting the subject's parameters"""
 			self.assertEqual(self.subject.name, 'SE')
 			self.assertEqual(self.subject.ID, 'INT1111')
 			self.assertEqual(self.subject.credits, 3)
@@ -36,16 +25,20 @@ class TestTheModels(TestCase):
 		
 	def test_adding_lecturer(self):
 		def setup(self):
+			"""adding a lecturer"""
 			self.lecturer = Lecturer(name='tuan anh', falcuty='IT')
 			
 		def test(self):
+			"""test asserting lecturer's parameters"""
 			self.assertEqual(self.lecturer.name, 'tuan anh')
 			self.assertEqual(self.lecturer.falcuty, 'IT')
 			
 		
 	def test_adding_scheduler(self):
 		def setup(self):
+			"""adding a scheduler"""
 			self.scheduler = Scheduler(name='April')
 			
 		def test(self):
+			"""asserting"""
 			self.assertEqual(self.scheduler.name, 'April')
