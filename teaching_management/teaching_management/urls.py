06b9ddbfcd1ad64e0d02a23477d8934db2404d0f
Nguyễn Hưng Thịnh <thinhnh56@gmail.com>
2013-05-15 23:08:40 +0700

    url

--- teaching_management/teaching_management/urls.py
+++ teaching_management/teaching_management/urls.py
@@ -1,8 +1,11 @@
+"""
+this assign funtions to handle urls 
+"""
 from django.conf.urls import patterns, include, url
-from django.contrib.auth.views import login, logout
+from django.contrib.auth.views import login
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
-from apps.views import profile, scheduler_link, scheduler, lecturer, subject, program, statistic,home
+from apps.views import profile, home, scheduler_link, scheduler, lecturer, subject, program, statistic, statistic_individual_lecturer
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -14,14 +17,18 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-	(r'^$', login),   	#link to login site
-	(r'subject$', subject),   #link to subject site
-	(r'lecturer$', lecturer),   #link to lecturer site
-	(r'program$', program),		#link to program site	
-	(r'accounts/profile/$', profile),	
-	(r'scheduler$', scheduler),	#link to scheduler site
-	(r'scheduler/([A-Za-z]+[^/]+)$', scheduler_link),	#link to the scheduler of each content of schedule
-	(r'statistic$', statistic),     	#link to statistic site
-	(r'^home$',home),					#link to homepage
+    (r'^$', login),       #link to login site
+    (r'subject$', subject),   #link to subject site
+    (r'lecturer$', lecturer),   #link to lecturer site
+    (r'program$', program),        #link to program site    
+    (r'accounts/profile/$', profile),    
+    (r'scheduler$', scheduler),    #link to scheduler site
+    (r'home$', home),
+    #link to the scheduler of each content of schedule
+    (r'scheduler/([A-Za-z]+[^/]+)$', scheduler_link),    
+    (r'statistic$', statistic),         #link to statistic site
+    
+    #link to statistic individual
+    (r'statistic/([A-Za-z]+[^/]+)$', statistic_individual_lecturer),
     url(r'^admin/', include(admin.site.urls)),  #link to admin site
 )
